1f73a544bddbf1adc9e4fa102367d69c3b44a729
Karen Sabile <karensabile224@gmail.com>
2023-10-23 22:09:30 -0400

    Fix buggy code for <lib/create_filled_restaurant>

--- lib/setting.ml
+++ lib/setting.ml
@@ -4,10 +4,11 @@ let rec read_int () =
     print_endline "Invalid input. Please enter an integer.";
     read_int ()
 
-(* Makes a 5 x 3 table (for 4 people) *)
+(* Makes a 5 x 3 table (for 4 people each). n is the number of tables in the
+   row. *)
 let create_table n =
   for i = 1 to 3 do
-    print_string "|";
+    print_string "|   ";
     for k = 1 to n do
       for j = 1 to 5 do
         if (i = 1 || i = 3) && (j = 1 || j = 5) then print_char ' '
@@ -15,7 +16,7 @@ let create_table n =
         else if j = 1 || j = 5 then print_char '|'
         else print_char ' '
       done;
-      if k < n then print_string "\t" else print_string "\t | \n"
+      if k < n then print_string "   " else print_string "   | \n"
     done
     (* print_string "\t |" *)
   done
@@ -24,17 +25,19 @@ let create_table n =
    that size with | and - with 5 x 3 size tables inside. [num_tables] represents
    the number of tables per row and and column of this square restaurant. *)
 let create_filled_restaurant num_tables =
-  let width = 5 * ((2 * num_tables) + 1) in
-  (* let height = 3 * ((2 * num_tables) + 1) in *)
+  let width = (5 * num_tables) + (3 * (num_tables - 1)) + 8 in
+  let border_string =
+    String.cat (String.cat "|" (String.make (width - 2) ' ')) "|\n"
+  in
   for i = 1 to width do
     print_char '-'
   done;
   print_newline ();
-  let border_string = 
+  print_string border_string;
   for j = 1 to num_tables do
     create_table num_tables;
-    print_string "\t | \n";
-    print_newline ()
+    print_string border_string;
+    print_string border_string
   done;
   for i = 1 to width do
     print_char '-'
