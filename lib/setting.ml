569b21e1d2daf39d805ac92242ccac2e6b1ce262
Karen Sabile <karensabile224@gmail.com>
2023-10-23 21:46:50 -0400

    Implement method <create_filled_restaurant> in lib/setting.ml
    - Modified previous code to create restaurant graphic with tables

--- lib/setting.ml
+++ lib/setting.ml
@@ -4,21 +4,47 @@ let rec read_int () =
     print_endline "Invalid input. Please enter an integer.";
     read_int ()
 
-(* Given the width and height, prints a rectangle of that size with | and -. *)
-let create_restaurant width height =
-  for i = 1 to height do
-    for j = 1 to width do
-      if (i = 1 || i = height) && (j = 1 || j = width) then print_char ' '
-      else if i = 1 || i = height then print_char '-'
-      else if j = 1 || j = width then print_char '|'
-      else print_char ' '
-    done;
-    print_newline ()
+(* Makes a 5 x 3 table (for 4 people) *)
+let create_table n =
+  for i = 1 to 3 do
+    print_string "|";
+    for k = 1 to n do
+      for j = 1 to 5 do
+        if (i = 1 || i = 3) && (j = 1 || j = 5) then print_char ' '
+        else if i = 1 || i = 3 then print_char '-'
+        else if j = 1 || j = 5 then print_char '|'
+        else print_char ' '
+      done;
+      if k < n then print_string "\t" else print_string "\t | \n"
+    done
+    (* print_string "\t |" *)
   done
 
+(* Given the number of tables horizontally and vertically, prints a rectangle of
+   that size with | and - with 5 x 3 size tables inside. [num_tables] represents
+   the number of tables per row and and column of this square restaurant. *)
+let create_filled_restaurant num_tables =
+  let width = 5 * ((2 * num_tables) + 1) in
+  (* let height = 3 * ((2 * num_tables) + 1) in *)
+  for i = 1 to width do
+    print_char '-'
+  done;
+  print_newline ();
+  let border_string = 
+  for j = 1 to num_tables do
+    create_table num_tables;
+    print_string "\t | \n";
+    print_newline ()
+  done;
+  for i = 1 to width do
+    print_char '-'
+  done;
+  print_newline ()
+
 let () =
-  print_string "Enter the width: ";
-  let width = read_int () in
-  print_string "Enter the height: ";
-  let height = read_int () in
-  create_restaurant width height
+  (* print_string "Enter the width: "; let width = read_int () in print_string
+     "Enter the height: "; let height = read_int () in create_restaurant width
+     height; *)
+  print_string "Enter the number of tables: ";
+  let num_tables = read_int () in
+  create_filled_restaurant num_tables
