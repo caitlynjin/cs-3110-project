4cce2fc2dd11e68f575798843e2afcb4de88aebb
Karen Sabile <karensabile224@gmail.com>
2023-11-29 13:42:03 -0500

    Add specs to functions in bin/main.ml
    - Modified some variable names for clarity and simplicity

--- bin/main.ml
+++ bin/main.ml
@@ -42,50 +42,61 @@ let rec read_key () =
    '*' done; print_newline (); for _ = 1 to width do print_char '-' done;
    print_newline () *)
 
+(* Initializes array representation of restaurant to an empty string array of
+   length [size], where [size] represents the height of the restaurant. *)
 let empty_restaurant size = Array.make size (ref "")
 
-let generate_line length c =
+(* Concatenates the substring [s] to the empty string [length] times, returning
+   a string that is [length] characters long. Requires [s] to be of length 1. *)
+let generate_line length s =
   let generate length acc =
     for _ = 1 to length do
-      acc := !acc ^ c
+      acc := !acc ^ s
     done;
     acc
   in
   generate length (ref "")
 
+(* Helper function to create rows for top and bottom edges of tables. *)
 let create_table_end row width =
   row := "|   ";
   for _ = 1 to width do
     row := !row ^ " ---    "
   done;
   row := !row ^ "|"
-(* let table_middle_row row width = row := "| "; for _ = 1 to width do row :=
-   !row ^ "| | " done; row := "|" *)
 
-let create_filled_restaurant2 num_tables =
-  let width = (5 * num_tables) + (3 * (num_tables - 1)) + 8 in
-  let height = num_tables * 5 in
-  let restaurant = empty_restaurant (height + 2) in
-  restaurant.(0) <- generate_line width "-";
+(* Modifies string array produced by function [empty_restaurant] to contain [n]
+   by [n] restaurant layout with [n * n] tables, and prints restaurant after
+   updating array. For example, passing [4] to create_filled_restaurant will
+   produce a restaurant of 4 x 4 = 16 tables. Because each table is a 5 * 3
+   square, ach iteration of the while loop modifies five elements of the
+   restaurant array. *)
+let create_filled_restaurant2 n =
+  let length = (5 * n) + (3 * (n - 1)) + 8 in
+  let width = n * 5 in
+  let restaurant = empty_restaurant (width + 2) in
+  restaurant.(0) <- generate_line length "-";
   let i = ref 1 in
-  while !i < height do
-    restaurant.(!i) <- ref ("|" ^ !(generate_line (width - 2) " ") ^ "|");
-    create_table_end restaurant.(!i + 1) num_tables;
-    restaurant.(!i + 2) <-
-      ref ("| " ^ !(generate_line num_tables "  |   | ") ^ "  |");
-    create_table_end restaurant.(!i + 3) num_tables;
-    restaurant.(!i + 4) <- ref ("|" ^ !(generate_line (width - 2) " ") ^ "|");
+  while !i < width do
+    restaurant.(!i) <- ref ("|" ^ !(generate_line (length - 2) " ") ^ "|");
+    create_table_end restaurant.(!i + 1) n;
+    restaurant.(!i + 2) <- ref ("| " ^ !(generate_line n "  |   | ") ^ "  |");
+    create_table_end restaurant.(!i + 3) n;
+    restaurant.(!i + 4) <- ref ("|" ^ !(generate_line (length - 2) " ") ^ "|");
     i := !i + 5
   done;
-  restaurant.(Array.length restaurant - 1) <- generate_line width "-";
+  restaurant.(Array.length restaurant - 1) <- generate_line length "-";
   for i = 0 to Array.length restaurant - 1 do
     print_string (!(restaurant.(i)) ^ "\n")
   done;
   ()
 
+(* Main function to handle user input and output. Queries user for the number of
+   tables they would like their restaurant to contain along its length and
+   width. *)
 let () =
   print_string "Enter the number of tables: ";
-  let n = read_int () in
+  let num_tables = read_int () in
   (* create_filled_restaurant1 n; *)
-  create_filled_restaurant2 n;
+  create_filled_restaurant2 num_tables;
   read_key ()
