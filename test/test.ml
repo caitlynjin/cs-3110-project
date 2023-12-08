9d85185c7bb0c4d9e96b9379c3827cbaede2ee02
span1012 <72399906+span1012@users.noreply.github.com>
2023-12-08 02:27:38 -0500

    Update test.ml

--- test/test.ml
+++ test/test.ml
@@ -1,145 +1,169 @@
-open OUnit2
-open Restaurant
-open Queue
 open Table
 
-(* open Menus *)
-open Points
+module Rest = struct
+  type t = string ref array array ref
 
-(* TEST PLAN
-Manually tested parts:
-- print statements (instructions, etc)
-- timer (delay in instructions, duration of parties, etc)
+  let restaurant_layout = ref (Array.make 0 (Array.make 0 (ref "")))
+  let table_list = ref []
+  let set_coord_symbol row col sym = row.(col) <- ref sym
+  let add_table_list id table = table_list := (id, table) :: !table_list
+  let get_table id = List.assoc id !table_list
 
-Automated tests:
-- queue functions
-- table functions
-- menu functions
-- points functions
+  (* changes the first nth seats of table id to sym *)
+  let change_seats_sym id n sym =
+    for i = 0 to n - 1 do
+      let x, y = List.nth (Table.coord_list (get_table id)) i in
+      set_coord_symbol !restaurant_layout.(x) y sym
+    done
 
-OUnit tested the Restaurant, Queue, Table, Menus, and Points modules. 
-Test cases were developed using glass box testing to ensure maximum coverage. 
-Therefore, more possible playouts of the game are tested and ensured that they 
-run the way we intend them to.
- *)
+  let width = ref 0
+  let height = ref 0
 
-let queue_tests =
-  [
-    ( "empty queue" >:: fun _ ->
-      assert_equal [] (WaitlistQueue.to_list WaitlistQueue.empty) );
-    ( "length of empty queue" >:: fun _ ->
-      assert_equal 0 (WaitlistQueue.length WaitlistQueue.empty) );
-    ( "length of nonempty queue" >:: fun _ ->
-      assert_equal 3
-        (WaitlistQueue.empty |> WaitlistQueue.enqueue 3
-       |> WaitlistQueue.enqueue 6 |> WaitlistQueue.enqueue 1
-       |> WaitlistQueue.length) );
-    ( "enqueue one element" >:: fun _ ->
-      assert_equal [ 3 ]
-        (WaitlistQueue.empty |> WaitlistQueue.enqueue 3 |> WaitlistQueue.to_list)
-    );
-    ( "enqueue multiple elements" >:: fun _ ->
-      assert_equal [ 3; 6; 1 ]
-        (WaitlistQueue.empty |> WaitlistQueue.enqueue 3
-       |> WaitlistQueue.enqueue 6 |> WaitlistQueue.enqueue 1
-       |> WaitlistQueue.to_list) );
-    ( "dequeue element from a nonempty queue" >:: fun _ ->
-      assert_equal (Some 3)
-        (WaitlistQueue.empty |> WaitlistQueue.enqueue 3
-       |> WaitlistQueue.enqueue 6 |> WaitlistQueue.enqueue 1
-       |> WaitlistQueue.dequeue) );
-    ( "dequeue element from an empty queue" >:: fun _ ->
-      assert_equal None (WaitlistQueue.empty |> WaitlistQueue.dequeue) );
-  ]
+  (* creates a restaurant based on the number of tables *)
+  let make_restaurant num_tables =
+    (* sets width and height *)
+    width := (5 * num_tables) + (3 * (num_tables - 1)) + 8;
+    height := (num_tables * 5) + 2;
 
-let ready_table = ReadyTable.make 0 3
-let occupied_table = OccupiedTable.make 4 8
-let dirty_table = DirtyTable.make 2 4
+    (* creates the tables *)
+    for n = 1 to num_tables * num_tables do
+      add_table_list n (Table.make 4 4)
+    done;
 
-let table_tests =
-  [
-    ( "ready table state" >:: fun _ ->
-      assert_equal "Ready" (ReadyTable.state ready_table) );
-    ( "ready table capacity" >:: fun _ ->
-      assert_equal 3 (ReadyTable.capacity ready_table) );
-    ( "ready table party size" >:: fun _ ->
-      assert_equal 0 (ReadyTable.party_size ready_table) );
-    ( "ready table is ready" >:: fun _ ->
-      assert_equal true (ReadyTable.isReady ready_table) );
-    ( "occupied table state" >:: fun _ ->
-      assert_equal "Occupied" (OccupiedTable.state occupied_table) );
-    ( "occupied table capacity" >:: fun _ ->
-      assert_equal 8 (OccupiedTable.capacity occupied_table) );
-    ( "occupied table party size" >:: fun _ ->
-      assert_equal 4 (OccupiedTable.party_size occupied_table) );
-    ( "occupied table is ready" >:: fun _ ->
-      assert_equal false (OccupiedTable.isReady occupied_table) );
-    ( "dirty table state" >:: fun _ ->
-      assert_equal "Dirty" (DirtyTable.state dirty_table) );
-    ( "dirty table capacity" >:: fun _ ->
-      assert_equal 4 (DirtyTable.capacity dirty_table) );
-    ( "dirty table party size" >:: fun _ ->
-      assert_equal 2 (DirtyTable.party_size dirty_table) );
-    ( "dirty table is ready" >:: fun _ ->
-      assert_equal true (DirtyTable.isReady dirty_table) );
-  ]
+    let current_table = ref 1 in
 
-(* let menu_test = [ ("menu test" >:: fun _ -> assert_equal "" !Menus.cuisine)
-   ] *)
-let gamepts = Points.game_pts
+    (* helper function to help generate row *)
+    let generate_row row i1 i2 c =
+      for i = i1 to i2 - 1 do
+        row.(i) <- c
+      done;
+      if i1 <> 0 then (
+        row.(0) <- ref "|";
+        row.(i2) <- ref "|")
+    in
 
-(* let pp_string s = "\"" ^ s ^ "\"" *)
-let pp_int i = "\"" ^ string_of_int i ^ "\""
+    (* creates the end of tables and adds coordiantes to each table in the
+       list *)
+    let create_table_end row width h =
+      (row.(0) <- ref "|";
+       row.(1) <- ref " ";
+       row.(2) <- ref " ";
+       row.(3) <- ref " ";
 
-let points_test =
-  [
-    (* points for queue management: parties_points *)
-    ( "short wait time" >:: fun _ ->
-      let _ = gamepts.queue_performance <- 0 in
-      let _ = Points.parties_points 5 3 in
-      assert_equal ~printer:pp_int 75 gamepts.queue_performance );
-    (* values accumulate?.. so set back to 0 *)
-    ( "medium wait time" >:: fun _ ->
-      let _ = gamepts.queue_performance <- 0 in
-      let _ = Points.parties_points 2 11 in
-      assert_equal ~printer:pp_int 20 gamepts.queue_performance );
-    ( "long wait time" >:: fun _ ->
-      let _ = gamepts.queue_performance <- 0 in
-      let _ = Points.parties_points 5 35 in
-      assert_equal ~printer:pp_int 25 gamepts.queue_performance );
-    (* points for treating parties at tables: meals_points *)
-    ( "short food time" >:: fun _ ->
-      let _ = gamepts.meals <- 0 in
-      let _ = Points.meals_points 1 1 1 1 in
-      assert_equal ~printer:pp_int 25 gamepts.meals );
-    ( "long food time" >:: fun _ ->
-      let _ = gamepts.meals <- 0 in
-      let _ = Points.meals_points 1 30 2 1 in
-      assert_equal ~printer:pp_int 15 gamepts.meals );
-    ( "long duration at table" >:: fun _ ->
-      let _ = gamepts.meals <- 0 in
-      let _ = Points.meals_points 5 1 1 40 in
-      assert_equal ~printer:pp_int 95 gamepts.meals );
-    ( "short duration at table" >:: fun _ ->
-      let _ = gamepts.meals <- 0 in
-      let _ = Points.meals_points 5 2 1 1 in
-      assert_equal ~printer:pp_int 45 gamepts.meals );
-    ( "medium food time" >:: fun _ ->
-      let _ = gamepts.meals <- 0 in
-      let _ = Points.meals_points 1 12 1 12 in
-      assert_equal ~printer:pp_int 17 gamepts.meals );
-    ( "medium duration" >:: fun _ ->
-      let _ = gamepts.meals <- 0 in
-      let _ = Points.meals_points 1 3 1 25 in
-      assert_equal ~printer:pp_int 30 gamepts.meals );
-    (* points from money generated: profits_points *)
-    ( "test profits" >:: fun _ ->
-      let _ = Points.profits_points 25. in
-      assert_equal ~printer:pp_int 625 gamepts.profits );
-  ]
+       let i = ref 4 in
+       while !i < width - 8 do
+         row.(!i) <- ref " ";
+         row.(!i + 1) <- ref "-";
+         row.(!i + 2) <- ref "-";
+         Table.add_list (get_table !current_table) h (!i + 2);
+         row.(!i + 3) <- ref "-";
+         row.(!i + 4) <- ref " ";
+         row.(!i + 5) <- ref " ";
+         row.(!i + 6) <- ref " ";
+         row.(!i + 7) <- ref " ";
+         i := !i + 8;
+         current_table := !current_table + 1
+       done;
+       row.(width - 1) <- ref "|");
+      current_table := !current_table - num_tables
+    in
 
-let suite =
-  "test suite for project"
-  >::: List.flatten [ queue_tests; table_tests; points_test ]
+    (* creates the middle row of the table (the | | of each table) *)
+    let table_middle_row row width h =
+      ((* sets the left border of the row *)
+       row.(0) <- ref "|";
+       row.(1) <- ref " ";
+       (* manually does the | | thing *)
+       let i = ref 2 in
+       while !i < width - 8 do
+         row.(!i) <- ref " ";
+         row.(!i + 1) <- ref " ";
+         row.(!i + 2) <- ref "|";
+         Table.add_list (get_table !current_table) h (!i + 2);
+         row.(!i + 3) <- ref " ";
+         row.(!i + 4) <- ref " ";
+         row.(!i + 5) <- ref " ";
+         row.(!i + 6) <- ref "|";
+         Table.add_list (get_table !current_table) h (!i + 6);
 
-let () = run_test_tt_main suite
+         row.(!i + 7) <- ref " ";
+         i := !i + 8;
+         current_table := !current_table + 1
+       done;
+       (* sets the right border of the row *)
+       row.(width - 3) <- ref " ";
+       row.(width - 2) <- ref " ";
+       row.(width - 1) <- ref "|");
+      current_table := !current_table - num_tables
+    in
+
+    restaurant_layout := Array.make !height (Array.make !width (ref ""));
+
+    for n = 0 to !height - 2 do
+      !restaurant_layout.(n) <- Array.make !width (ref "")
+    done;
+
+    (* restaurant_layout.(0) <- ref "\n ~Dish Dash Dilemma!"; *)
+
+    (* first sets the first row of the array to just "-" *)
+    generate_row !restaurant_layout.(0) 0 !width (ref "-");
+    let i = ref 1 in
+    while !i < !height - 2 do
+      generate_row !restaurant_layout.(!i) 1 (!width - 1) (ref " ");
+      create_table_end !restaurant_layout.(!i + 1) !width (!i + 1);
+      table_middle_row !restaurant_layout.(!i + 2) !width (!i + 2);
+      create_table_end !restaurant_layout.(!i + 3) !width (!i + 3);
+      generate_row !restaurant_layout.(!i + 4) 1 (!width - 1) (ref " ");
+      i := !i + 5;
+      current_table := !current_table + num_tables
+    done;
+    (* sets the next to last row to an empty row and the final row of the array
+       to just "-" *)
+    generate_row !restaurant_layout.(!height - 2) 1 (!width - 1) (ref " ");
+    generate_row !restaurant_layout.(!height - 1) 0 !width (ref "-")
+
+  let display_filled_restaurant () =
+    (* prints everything out *)
+    for i = 0 to !height - 1 do
+      for j = 0 to !width - 1 do
+        print_string !(!restaurant_layout.(i).(j))
+      done;
+      print_string "\n"
+    done;
+    (* where print queue starts *)
+    for _ = 0 to Random.int 10 do
+      print_string "* "
+    done;
+    print_endline "\n"
+
+  (* prints out the table list such that "table #'s coordinates are (#,
+     #)(#,#)..." *)
+  let print_list () =
+    let print_coord acc (h, w) =
+      acc ^ "(" ^ string_of_int h ^ ", " ^ string_of_int w ^ ")"
+    in
+    let print_coord_list lst = List.fold_left print_coord "" lst in
+    let print_entry (id, table) =
+      print_string
+        ("table " ^ string_of_int id ^ "'s coordinates are "
+        ^ print_coord_list (Table.coord_list table)
+        ^ "\n")
+    in
+    List.iter print_entry !table_list
+
+  (* seat_party will modify the corresponding rows of table_id and place 'x's
+     around that table to represent the people in the party. [num_people] = the
+     number of x's to place around the table [table_id] = the table to place the
+     people at *)
+  let seat_party num_people table_id =
+    if Table.isReady (get_table table_id) then
+      if num_people > Table.capacity (get_table table_id) then
+        failwith "too much people"
+      else (
+        Table.seat (get_table table_id) num_people;
+        change_seats_sym table_id num_people "*")
+    else failwith "Table is not ready"
+
+  let get_table_list = !table_list
+  let get_coord_value x y = !(!restaurant_layout.(x).(y))
+end
